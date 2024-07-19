bb09fa3cfda55a42cfefca99a645435b48b60987
sxngt <sxngt@icloud.com>
2024-07-19 15:13:32 +0900

    인증인가 로직 삭제 (대회 규정)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infrastructure/database.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.develop.env',
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
--- src/common/decorators/get-user-data.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../../domains/user/entities/user.entity';
-export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): User => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
